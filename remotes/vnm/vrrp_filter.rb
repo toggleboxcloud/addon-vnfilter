e62e1a6e844b68d112a6ff8c0d399c32d65bec3d
Togglebox OpenNebula Production <oneadmin@cloud.togglebox.com>
2026-07-23 16:04:37 -0400

    Compose production vnfilter and VRRP sources

---
+++ remotes/vnm/vrrp_filter.rb
@@ -0,0 +1,396 @@
+# rubocop:disable Naming/FileName
+# vim: ts=4 sw=4 et
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2022, StorPool                                              #
+# Portion copyright OpenNebula Project, OpenNebula Systems                   #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+require 'vnmmad'
+require 'syslog/logger'
+
+# IP filter for aliases
+class VnFilter < VNMMAD::VNMDriver
+
+    DRIVER = 'vnfilter'
+    XPATH_FILTER = 'TEMPLATE/NIC|TEMPLATE/NIC_ALIAS'
+
+    def initialize(vm_template, xpath_filter = nil, deploy_id = nil)
+        @locking = true
+        @slog = Syslog::Logger.new 'vnfilter'
+        xpath_filter ||= XPATH_FILTER
+        @slog.info "initialize #{xpath_filter} //#{caller[-1]}"
+        super(vm_template, xpath_filter, deploy_id)
+    end
+
+    def append_ebtables(chain, ipv4)
+        @slog.info "activate_ebtables(#{chain},#{ipv4})"
+        dirs = { "i" => "src", "o" => "dst" }
+        ret = false
+        commands =  VNMMAD::VNMNetwork::Commands.new
+        commands.add "sudo -n", "ebtables-save"
+        ebtables_nat = commands.run!
+        if !ebtables_nat.nil?
+            ebtables_nat.split("\n").each do |rule|
+                if rule.match(/-A #{chain}-([io]{1})-arp4/)
+                    dir = $+
+                    rule_e = rule.split
+                    ip = rule_e[5]
+                    if ipv4 == rule_e[5]
+                        @slog.info "[match] #{rule} // #{ip} #{dir}"
+                        dirs.delete(dir)
+                        ret = true
+                    end
+                end
+            end
+        end
+        if dirs.any?
+            dirs.each do |k,v|
+                @slog.info "whitelist arp-ip-#{v} #{ipv4} (#{k})"
+                commands.add :ebtables, "-t nat -A #{chain}-#{k}-arp4 -p ARP "\
+                                            "--arp-ip-#{v} #{ipv4} -j RETURN"
+                ret = true
+            end
+            commands.run!
+        end
+        return ret
+    end
+
+    def activate
+        ipv4_offset = 2
+        ipv6_offset = 5
+        lock
+        vm_id = vm['ID']
+        attach_nic_id = vm['TEMPLATE/NIC[ATTACH="YES"]/NIC_ID']
+        parent_id = vm['TEMPLATE/NIC_ALIAS[ATTACH="YES"]/PARENT_ID']
+        caller_mad = caller[-1].split('/')[-3]
+        if parent_id
+            parent_mac_spoofing = vm["TEMPLATE/NIC[NIC_ID=#{parent_id}]/FILTER_MAC_SPOOFING"]
+            if !parent_mac_spoofing.nil? && !parent_mac_spoofing.empty?
+                if parent_mac_spoofing.upcase! != 'YES'
+                    @slog.warn "activate() VM #{vm_id} Warning: parent NIC_ID #{parent_id} has FILTER_MAC_SPOOFING=#{parent_mac_spoofing}! //SKIP"
+                    unlock
+                    return
+                end
+            else
+                @slog.warn "activate() VM #{vm_id} Warning: no FILTER_MAC_SPOOFING enabled on parent NIC_ID #{parent_id}! //SKIP"
+                unlock
+                return
+            end
+            ipv4 = vm['TEMPLATE/NIC_ALIAS[ATTACH="YES"]/IP']
+            if ipv4
+                @slog.info "activate() VM #{vm_id} parent_id:#{parent_id} BEGIN"
+                chain = "one-#{vm_id}-#{parent_id}"
+                if append_ebtables(chain, ipv4)
+                    @slog.info "activate() VM #{vm_id} parent_id:#{parent_id} END"
+                    unlock
+                    return
+                end
+            end
+        end
+        @slog.info "activate() VM #{vm_id} (#{attach_nic_id}) parent_id:#{parent_id} BEGIN"
+        # pre-process
+        nics = Hash.new
+        process do |nic|
+            nic_id = nic[:nic_id]
+            ip4 = Array.new
+            ip6 = Array.new
+            [:ip, :vrouter_ip].each do |key|
+                if !nic[key].nil? && !nic[key].empty?
+                    ip4 << nic[key]
+                end
+            end
+            [:ip6, :ip6_global, :ip6_link].each do |key|
+                # Skip IPv6 link local address for alias interfaces
+                next if !nic[:alias_id].nil? && key == "ip6_link"
+                if !nic[key].nil? && !nic[key].empty?
+                    ipv6net = nic[key]
+                    ipv6net += "/#{nic[:ipset_prefix_length]}"\
+                        if key == :ip6 && !nic[:ipset_prefix_length].nil? &&\
+                           !nic[:ipset_prefix_length].empty?
+                    ip6 << ipv6net
+                end
+            end
+            if !nic[:alias_id].nil?
+                parent_id = nic[:parent_id]
+                if nics[parent_id].nil?
+                    nics[parent_id] = Hash.new
+                    nics[parent_id][:ip4] = Array.new
+                    nics[parent_id][:ip6] = Array.new
+                end
+                nics[parent_id][:ip4].push(*ip4)
+                nics[parent_id][:ip6].push(*ip6)
+                next
+            end
+            if nics[nic_id].nil?
+                nics[nic_id] = Hash.new
+                nics[nic_id][:ip4] = ip4
+                nics[nic_id][:ip6] = ip6
+            else
+                nics[nic_id][:ip4].push(*ip4)
+                nics[nic_id][:ip6].push(*ip6)
+            end
+            nics[nic_id][:nic] = nic
+        end
+
+        nics.each do |nic_id, nicdata|
+            nic = nicdata[:nic]
+            vn_mad = nic[:vn_mad]
+            if caller_mad != vn_mad
+                @slog.info "VM #{vm_id} nic_id #{nic_id} #{vn_mad} Skip caller VN_MAD is #{caller_mad}"
+                next
+            end
+            @slog.info "VM #{vm_id} nic_id #{nic_id} attach_nic_id:#{attach_nic_id}"
+            OpenNebula.log_info "VM #{vm_id} nic_id #{nic_id} #{vn_mad} attach_nic_id #{attach_nic_id}"
+            next if attach_nic_id and attach_nic_id != nic_id
+            chain = "one-#{vm_id}-#{nic_id}"
+            chain_i = "#{chain}-i"
+            chain_o = "#{chain}-o"
+
+            commands =  VNMMAD::VNMNetwork::Commands.new
+
+            if nic[:filter_ip_spoofing] == "YES"
+                @slog.info "VM #{vm_id} NIC #{nic_id} FILTER_IP_SPOOFING"
+                commands.add :iptables, "-S #{chain_o}"
+                begin
+                    iptables_s = commands.run!
+                rescue
+                    @slog.warn "Can't process chain #{chain_o} IPv4"
+                    next
+                end
+                iptables_s.each_line { |c| @slog.info "[iptables -S] #{c}" }
+                if iptables_s !~ /#{chain}-ip-spoofing/
+                    @slog.info "patching #{chain_o} to add #{chain}-ip-spoofing"
+                    commands.add :ipset, "create -exist #{chain}-ip-spoofing hash:ip family inet"
+                    commands.add :iptables, "-R #{chain_o} #{ipv4_offset} -m set ! --match-set #{chain}-ip-spoofing src -j DROP"
+                    commands.add :iptables, "-I #{chain_o} #{ipv4_offset} -s 0.0.0.0/32 -d 255.255.255.255/32 -p udp -m udp --sport 68 --dport 67 -j RETURN"
+                end
+                if !nicdata[:ip4].nil? and !nicdata[:ip4].empty?
+                    nicdata[:ip4].each do |ip|
+                        @slog.info "ipset add #{chain}-ip-spoofing #{ip}"
+                        commands.add :ipset, "add -exist #{chain}-ip-spoofing #{ip}"
+                    end
+                    commands.run!
+                end
+                commands.add :ip6tables, "-S #{chain_o}"
+                begin
+                    ip6tables_s = commands.run!
+                rescue
+                    @slog.warn "Can't process chain #{chain_o} IPv6"
+                    next
+                end
+                if !nicdata[:ipset_prefix_length].nil? &&
+                    !nicdata[:ipset_prefix_length].empty?
+                    ipset_hash = "hash:net"
+                else
+                    ipset_hash = "hash:ip"
+                end
+                ip6tables_s.each_line { |c| @slog.info "[ip6tables -S] #{c}" }
+                if ip6tables_s !~ /#{chain}-ip6-spoofing/
+                    @slog.debug "altering #{chain_o} to add #{chain}-ip6-spoofing"
+                    commands.add :ipset, "create -exist #{chain}-ip6-spoofing #{ipset_hash} family inet6"
+                    commands.add :ip6tables, "-R #{chain_o} #{ipv6_offset} -m set ! --match-set #{chain}-ip6-spoofing src -j DROP"
+                end
+                if !nicdata[:ip6].nil? and !nicdata[:ip6].empty?
+                    nicdata[:ip6].each do |ipv6|
+                        @slog.info "ipset add #{chain}-ip6-spoofing #{ipv6}"
+                        commands.add :ipset, "add -exist #{chain}-ip6-spoofing #{ipv6}"
+                    end
+                    commands.run!
+                end
+            end
+
+            if nic[:filter_mac_spoofing] == "YES"
+                @slog.info "VM #{vm_id} NIC #{nic_id} FILTER_MAC_SPOOFING"
+                deactivate_ebtables(chain)
+                commands.add :ebtables, "-t nat -N #{chain_i}-arp4 -P DROP"
+                commands.add :ebtables, "-t nat -N #{chain_o}-arp4 -P DROP"
+                if !nicdata[:ip4].nil? and !nicdata[:ip4].empty?
+                    nicdata[:ip4].each do |ip|
+                        @slog.info "ARP whitelist #{ip} (#{chain})"
+                        commands.add :ebtables, "-t nat -A #{chain_i}-arp4 -p ARP "\
+                            "--arp-ip-src #{ip} -j RETURN"
+                        commands.add :ebtables, "-t nat -A #{chain_o}-arp4 -p ARP "\
+                            "--arp-ip-dst #{ip} -j RETURN"
+                    end
+                end
+                # Input
+                commands.add :ebtables, "-t nat -N #{chain_i}-arp -P DROP"
+                commands.add :ebtables, "-t nat -A #{chain_i}-arp -p ARP "\
+                    "-s ! #{nic[:mac]} -j DROP"
+                commands.add :ebtables, "-t nat -A #{chain_i}-arp -p ARP "\
+                        "--arp-mac-src ! #{nic[:mac]} -j DROP"
+                commands.add :ebtables, "-t nat -A #{chain_i}-arp -p ARP "\
+                    "-j #{chain_i}-arp4"
+                commands.add :ebtables, "-t nat -A #{chain_i}-arp -p ARP "\
+                    "--arp-op Request -j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_i}-arp -p ARP "\
+                    "--arp-op Reply -j ACCEPT"
+                commands.add :ebtables, "-t nat -N #{chain_i}-rarp -P DROP"
+                commands.add :ebtables, "-t nat -A #{chain_i}-rarp -p 0x8035 "\
+                    "-s #{nic[:mac]} -d Broadcast --arp-op Request_Reverse "\
+                    "--arp-ip-src 0.0.0.0 --arp-ip-dst 0.0.0.0 "\
+                    "--arp-mac-src #{nic[:mac]} --arp-mac-dst #{nic[:mac]} "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -N #{chain_i} -P ACCEPT"
+#            commands.add :ebtables, "-t nat -N #{chain_i}-ip4 -P ACCEPT"
+#            commands.add :ebtables, "-t nat -A #{chain_i}-ip4 "\
+#                "-s ! #{nic[:mac]} -j DROP"
+#            commands.add :ebtables, "-t nat -A #{chain_i} -p IPv4 "\
+#                "-j #{chain_i}-ip4"
+                commands.add :ebtables, "-t nat -A #{chain_i} -p IPv4 "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_i} -p IPv6 "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_i} -p ARP "\
+                    "-j #{chain_i}-arp"
+                commands.add :ebtables, "-t nat -A #{chain_i} -p 0x8035 "\
+                    "-j #{chain_i}-rarp"
+                commands.add :ebtables, "-t nat -A PREROUTING -i #{chain} "\
+                    "-j #{chain_i}"
+                # Output
+                commands.add :ebtables, "-t nat -N #{chain_o}-arp -P DROP"
+                commands.add :ebtables, "-t nat -A #{chain_o}-arp -p ARP "\
+                    "--arp-op Reply --arp-mac-dst ! #{nic[:mac]} -j DROP"
+                commands.add :ebtables, "-t nat -A #{chain_o}-arp -p ARP "\
+                    "-j #{chain_o}-arp4"
+                commands.add :ebtables, "-t nat -A #{chain_o}-arp -p ARP "\
+                    "--arp-op Request -j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_o}-arp -p ARP "\
+                    "--arp-op Reply -j ACCEPT"
+                commands.add :ebtables, "-t nat -N #{chain_o}-rarp -P DROP"
+                commands.add :ebtables, "-t nat -A #{chain_o}-rarp -p 0x8035 "\
+                    "-d Broadcast --arp-op Request_Reverse "\
+                    "--arp-ip-src 0.0.0.0 --arp-ip-dst 0.0.0.0 "\
+                    "--arp-mac-src #{nic[:mac]} --arp-mac-dst #{nic[:mac]} "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -N #{chain_o} -P ACCEPT"
+#            commands.add :ebtables, "-t nat -N #{chain_o}-ip4 -P ACCEPT"
+#            commands.add :ebtables, "-t nat -A #{chain_o} -p IPv4 "\
+#                "-j #{chain_o}-ip4"
+                commands.add :ebtables, "-t nat -A #{chain_o} -p IPv4 "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_o} -p IPv6 "\
+                    "-j ACCEPT"
+                commands.add :ebtables, "-t nat -A #{chain_o} -p ARP "\
+                    "-j #{chain_o}-arp"
+                commands.add :ebtables, "-t nat -A #{chain_o} -p 0x8035 "\
+                    "-j #{chain_o}-rarp"
+                commands.add :ebtables, "-t nat -A POSTROUTING -o #{chain} "\
+                    "-j #{chain_o}"
+
+                commands.run!
+            end
+        end
+        @slog.info "activate() VM #{vm_id} END"
+        unlock
+    end
+
+    def deactivate
+        lock
+        vm_id = vm['ID']
+        caller_mad = caller[-1].split('/')[-3]
+        @slog.info "deactivate() VM #{vm_id} caller_mad:#{caller_mad} BEGIN"
+        res = false
+        attach = false
+        nics = Hash.new
+        process do |nic|
+            next if caller_mad != nic[:vn_mad]
+            nic_id = nic[:nic_id]
+            chain = "one-#{vm_id}-#{nic_id}"
+            if nic[:attach]
+                @slog.info "VM #{vm_id} NIC #{nic_id} vn_mad=#{nic[:vn_mad]} parent=#{nic[:parent]} ip=#{nic[:ip]}"
+                attach = true
+                if nic[:parent].nil?
+                    deactivate_ebtables(chain)
+                else
+                    deactivate_ebtables(chain, nic[:ip]) if !nic[:ip].nil?
+                end
+            else
+                if nic[:parent].nil?
+                    nics[nic_id] = nic
+                end
+            end
+        end
+        if !attach
+            nics.each do |nic_id, nic|
+                @slog.info "VM #{vm_id} NIC #{nic_id} vn_mad=#{nic[:vn_mad]} down"
+                deactivate_ebtables("one-#{vm_id}-#{nic_id}")
+            end
+        end
+        @slog.info "deactivate() VM #{vm_id} END"
+        unlock
+    end
+
+    def deactivate_ebtables(chain, ipv4 = nil)
+        commands =  VNMMAD::VNMNetwork::Commands.new
+        @slog.info "deactivate_ebtables(#{chain}, #{ipv4})"
+        commands.add "sudo -n", "ebtables-save"
+        ebtables_nat = commands.run!
+        if !ebtables_nat.nil?
+            ebtables = Array.new
+            chains = Array.new
+            ebtables_nat.split("\n").each do |rule|
+                if ipv4
+                    if rule.match(/-A #{chain}/)
+                        rule_e = rule.split
+                        @slog.info "[rule] #{rule}"
+                        if rule_e[5] == ipv4
+                            @slog.info "Delete #{rule}"
+                            ebtables.push("-t nat -D #{rule_e[1..-1].join(" ")}")
+                        end
+                    end
+                    next
+                end
+
+                # flush chains only if not ipv4 defined (no alias nic)
+                if rule.match(/-j #{chain}/)
+                    rule_e = rule.split
+                    @slog.info "[rule] #{rule}"
+                    if rule_e[2] == "-p"
+                        ebtables.push("-t nat -F #{rule_e[-1]}")
+                        ebtables.push("-t nat -X #{rule_e[-1]}")
+                        ebtables.unshift("-t nat -D #{rule_e[1..-1].join(" ")}")
+                    else
+                        ebtables.push("-t nat -D #{rule_e[1..-1].join(" ")}")
+                        ebtables.push("-t nat -F #{rule_e[-1]}")
+                        ebtables.push("-t nat -X #{rule_e[-1]}")
+                    end
+                end
+
+                # save any chains found
+                if rule.match(/:#{chain}/)
+                    rule_e = rule.split
+                    c = rule_e[0][1..-1]
+                    @slog.info "[chain] #{c}"
+                    chains.push(c)
+                end
+            end
+
+            chains.each do |c|
+                unless ebtables.include? "-t nat -X #{c}"
+                    ebtables.push("-t nat -F #{c}")
+                    ebtables.push("-t nat -X #{c}")
+                end
+            end
+
+            if ebtables.any?
+                ebtables.each { |c| @slog.info "[run] ebtables #{c}" }
+                ebtables.each { |c| commands.add :ebtables, c }
+                commands.run!
+            end
+        end
+    end
+
+end
